71f85d3530ccec432c49b69070aa76a35113a2d4
Lars Beer <driemtax@gmail.com>
2025-03-06 19:19:01 +0100

    Exit command with arg 0 fourth test

--- app/main.go
+++ app/main.go
@@ -23,6 +23,7 @@ func main() {
 
 		switch command {
 		case "exit 0":
+			fmt.Println("Ending the program..")
 			os.Exit(0)
 		default:
 			fmt.Println(command[:len(command)-1] + ": command not found")
