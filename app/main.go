71f1b5cd0703a2117a4c5a847b9ec1bd0e523ff0
Lars Beer <driemtax@gmail.com>
2025-03-06 19:04:10 +0100

    FIrst test of REPL

--- app/main.go
+++ app/main.go
@@ -10,15 +10,17 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
-	}
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	fmt.Println(command[:len(command)-1] + ": command not found")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
 
+		fmt.Println(command[:len(command)-1] + ": command not found")
+		}
 }
