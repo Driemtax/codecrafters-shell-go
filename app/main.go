3e1ab30fba8725dae00de1479654bc5c71545bfc
Lars Beer <driemtax@gmail.com>
2025-03-06 18:54:16 +0100

    Handling invalid commands

--- app/main.go
+++ app/main.go
@@ -12,6 +12,10 @@ var _ = fmt.Fprint
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
+	command, err := bufio.NewReader(os.Stdin).ReadString("\n")
+
+	fmt.Println(command[:len(command)-1] + " command not found")
+
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
