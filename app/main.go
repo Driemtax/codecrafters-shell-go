571287ead20d71c6a0d3df664883e93f0d654a42
Lars Beer <driemtax@gmail.com>
2025-03-08 13:03:36 +0100

    type command first test

--- app/main.go
+++ app/main.go
@@ -13,6 +13,9 @@ var _ = fmt.Fprint
 func main() {
 
 	for {
+		// Commands currently bultin
+		commands := [3]string{"echo", "exit", "type"}
+
 		fmt.Fprint(os.Stdout, "$ ")
 
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -24,14 +27,42 @@ func main() {
 
 		command = strings.TrimSpace(command)
 
+		// Evaluate the given command and args
 		switch {
+		// Exit the shell
 		case command == "exit 0":
 			os.Exit(0)
+		// echo your input to the console
 		case strings.HasPrefix(command, "echo"):
 			var output string = strings.TrimPrefix(command, "echo ")
 			fmt.Println(output)
+		// type shows how the given command would be interpreted
+		case strings.HasPrefix(command, "type"):
+			var arg string = strings.TrimPrefix(command, "type ")
+			if checkCommands(commands, arg) {
+				fmt.Println(arg + " is a shell builtin")
+			} else {
+				fmt.Println(arg + ": not found")
+			}
 		default:
 			fmt.Println(command + ": command not found")
 		}
 	}
+}
+
+// checks if a given command is a built in command in this shell
+// commands is the array of builtin commands
+// arg is the given command
+// returns a bool if the command is builtin or not
+func checkCommands(commands [3]string, arg string) bool {
+	found := false
+
+	for _, cmd := range commands {
+		if cmd == arg {
+			found = true
+			break
+		}
+	}
+
+	return found
 }
