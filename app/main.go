f5172dc8ec1d741ed0e631ae12a464086e5b1c04
Lars Beer <driemtax@gmail.com>
2025-03-08 16:21:18 +0100

    searching path variable first test

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -39,11 +40,16 @@ func main() {
 		// type shows how the given command would be interpreted
 		case strings.HasPrefix(command, "type"):
 			var arg string = strings.TrimPrefix(command, "type ")
+			var envPath string = checkEnvs(arg)
 			if checkCommands(commands, arg) {
 				fmt.Println(arg + " is a shell builtin")
+			} else if envPath != "" {
+				fmt.Println(arg + " is " + envPath)
 			} else {
 				fmt.Println(arg + ": not found")
 			}
+		case command == "test":
+			checkEnvs(command)
 		default:
 			fmt.Println(command + ": command not found")
 		}
@@ -65,4 +71,63 @@ func checkCommands(commands [3]string, arg string) bool {
 	}
 
 	return found
+}
+
+// checks for the cmd in the PATH env
+// cmd is the command that was typed in
+// returns the path of the cmd if found, else just an empty string
+func checkEnvs(cmd string) string {
+	var path string = ""
+
+	fmt.Println("PATH: " + os.Getenv("PATH"))
+
+	var paths []string = strings.Split(os.Getenv("PATH"), ":")
+
+	for _, dir := range paths {
+		found, err := findExecutable(dir, cmd)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue // needed to continue instead of return if there is an error opening a single dir
+		}
+
+		if found {
+			path = dir + "/" + cmd // not the cleanest way but i needed a string instance
+			return path
+		}
+
+	}
+
+	return path
+}
+
+// Finds an executable file in a given dir
+// path is the given directory
+// fileName is the executable you are looking for
+// returns if there was an executable found with the given fileName
+func findExecutable(path string, fileName string) (bool, error) {
+	// Read all entries in given directory
+	entries, err := os.ReadDir(path)
+
+	if err != nil {
+		return false, err
+	}
+
+	for _, entry := range entries {
+		// check if is not a dir and file name is searched one
+		if !entry.IsDir() && entry.Name() == fileName {
+			info, err := os.Stat(filepath.Join(path, fileName))
+
+			if err != nil {
+				return false, err
+			}
+
+			// Check if file is an executable
+			if (info.Mode() & 0111) != 0 {
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
 }
